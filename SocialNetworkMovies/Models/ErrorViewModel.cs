a4773ea0df8e4a64c756084dd17b0c32d77971ab
pedroreal12 <pedrofilipereal12@gmail.com>
2023-12-06 09:32:34 +0000

    initial commit

---
+++ SocialNetworkMovies/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SocialNetworkMovies.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
