63af1b02fbb5e4635575c7786e22d0c204e9617b
pedroreal12 <pedrofilipereal12@gmail.com>
2023-12-26 17:53:01 +0000

    fix: not using the projects models in the dbcontext file

--- SocialNetworkMovies/Models/sndbContext.cs
+++ SocialNetworkMovies/Models/sndbContext.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using Microsoft.EntityFrameworkCore;
+using SocialNetworkMovies.Models;
 
 namespace SocialNetworkMovies.Models;
 
@@ -32,7 +33,6 @@ public partial class SndbContext : DbContext
     public virtual DbSet<UserList> UserLists { get; set; }
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
         => optionsBuilder.UseSqlServer("Server=localhost, 1433;Database=sndb;User Id=SA;Password=A&VeryComplex123Password;TrustServerCertificate=True;MultipleActiveResultSets=true");
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
