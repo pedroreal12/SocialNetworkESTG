1c313d08a63dd1cdb0a0bf244626de7333c4c49b
pedroreal12 <pedrofilipereal12@gmail.com>
2023-12-28 23:14:52 +0000

    feat: added a search in the navbar for movies

--- SocialNetworkMovies/wwwroot/js/site.js
+++ SocialNetworkMovies/wwwroot/js/site.js
@@ -1,4 +1,30 @@
-﻿// Please see documentation at https://learn.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿$(document).ready(function() {
+    $("#btnSearch").click(function() {
+        event.preventDefault()
+        $.ajax({
+            url: "/Movies/GetMovieByTitle/?title=" + $("#inputSearch").val(),
+            type: "GET",
+            success: function(data) {
+                var content = JSON.parse(data.content)
+                if (content.results.length > 0) {
+                    var html = "<h5>Search Results</h5>"
+                    content.results.forEach(function(element){
+                        html += "<a href=\"/Movies/MovieDetails/" + element.id + "\">" + element.original_title + " - " + element.release_date + "</a><br>"
+                    })
+                    html += "<button class=\"btn btn-secondary\" onClick=\"clearSearches()\">Clear Searches</button>"
+                    $(".searchResults").append(html)
+                } else {
+                    alert("No movies were found. Try later or with a different title")
+                }
+            },
+            error: function(error) {
+                alert("Error: " + error)
+            }
+        })
+    })
+})
 
-// Write your JavaScript code.
+function clearSearches(){
+    $(".searchResults").children().remove()
+    $("#inputSearch").val("")
+}
