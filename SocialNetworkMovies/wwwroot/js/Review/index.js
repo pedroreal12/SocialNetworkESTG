9d918fea4395f865aba1d87e8da359b0bd9a90e9
pedroreal12 <pedrofilipereal12@gmail.com>
2023-12-30 14:26:27 +0000

    fix: addded formatDate to the reviews index.js

--- SocialNetworkMovies/wwwroot/js/Review/index.js
+++ SocialNetworkMovies/wwwroot/js/Review/index.js
@@ -12,7 +12,7 @@ function loadReviews() {
             var content = JSON.parse(data)
             if (content.length > 0) {
                 content.forEach(function(element) {
-                    $(".listReviews").append("<a href=\"/Review/Details/" + element.Id + "\">" + element.Text + " - Posted at " + element.DatePosted + "</a><br>")
+                    $(".listReviews").append("<a href=\"/Review/Details/" + element.Id + "\">" + element.Text + " - Posted at " + formatDate(element.DatePosted) + "</a><br>")
                 })
                 $(".listReviews").append("<button class=\"btn btn-link\" onClick=\"loadReviews()\" id=\"loadReviews\">Load more reviews</button>")
                 Pagination += 1
@@ -24,4 +24,15 @@ function loadReviews() {
             alert("Error: " + error)
         }
     })
+}
+
+function formatDate(dateObj) {
+    var formattedDate = new Date(dateObj);
+    var d = formattedDate.getDate();
+    var m = formattedDate.getMonth();
+    m += 1;  // JavaScript months are 0-11
+    var y = formattedDate.getFullYear();
+    var formatedDate = d + "/" + m + "/" + y
+
+    return formatedDate
 }
